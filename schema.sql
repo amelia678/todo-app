951a9242f2f4e81e026b7531868f30f9dae3f986
Amelia Schulz <you@example.com>
2018-11-05 10:45:48 -0500

    adds database setup files and npm scripts

---
+++ schema.sql
@@ -0,0 +1,10 @@
+-- TODOs
+-- title
+-- completed
+
+create table todos (
+    id serial primary key,
+    name text,
+    completed boolean
+);
+
---
+++ seed.sql
@@ -0,0 +1,8 @@
+insert into todos
+    (name, completed)
+values
+    ('walk the cat', false),
+    ('make stacks', false),
+    ('coven meeting', false),
+    ('turn back time', false),
+    ('meeting with the devil', false);
